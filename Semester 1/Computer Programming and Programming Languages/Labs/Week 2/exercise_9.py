00ad27ab310de9a3a8a522710693ead322adcc6b
AndreeaSofia <144051543+AndreeaSofia@users.noreply.github.com>
2024-10-23 22:29:10 +0300

    Update exercise_9.py

--- Semester 1/Computer Programming and Programming Languages/Labs/Week 2/exercise_9.py
+++ Semester 1/Computer Programming and Programming Languages/Labs/Week 2/exercise_9.py
@@ -1,5 +1,5 @@
 # Print all numbers with maximum 2 digits of form xy with the property that the last digit of (xy)^2 is y.
-# (e.g.: 5^2=25 or 10^2=100 or 76^2=5776)/
+# (e.g.: 5^2=25 or 10^2=100 or 76^2=5776).
 
 
 def find_numbers_xy():
