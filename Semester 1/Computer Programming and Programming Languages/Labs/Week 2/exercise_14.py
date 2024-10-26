ee141ce2ea2e4cef3b327579cb83893f2adbd47f
AndreeaSofia <144051543+AndreeaSofia@users.noreply.github.com>
2024-10-27 02:07:34 +0300

    Update exercise_14.py

--- Semester 1/Computer Programming and Programming Languages/Labs/Week 2/exercise_14.py
+++ Semester 1/Computer Programming and Programming Languages/Labs/Week 2/exercise_14.py
@@ -1,4 +1,4 @@
-def get_count_of_ones_from_binery_representation_of_number(n):
+def get_count_of_ones_from_binary_representation_of_number(n):
     count = 0
     while n > 0:
         count = count + n % 2
